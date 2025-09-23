36c754a67ed792d885d7f62a942544bb75931f79
yaren <136489994+yarennoztekinn@users.noreply.github.com>
2025-09-23 15:41:23 +0300

    project's main structure is created. closes #8

---
+++ backend/src/main/java/com/gangofthree/tarladan/TarladanApplication.java
@@ -0,0 +1,13 @@
+package com.gangofthree.tarladan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarladanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarladanApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gangofthree/tarladan/TarladanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangofthree.tarladan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarladanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

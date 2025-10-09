5b58ba0f9292f59eb52c9e7a2ba939927482969e
yaren <136489994+yarennoztekinn@users.noreply.github.com>
2025-10-09 17:12:20 +0300

    ciftcinin urun ekleme feature'u eklendi. fix# 25

--- backend/src/main/java/com/gangofthree/tarladan/modules/farmer/controller/FarmerController.java
+++ backend/src/main/java/com/gangofthree/tarladan/modules/farmer/controller/FarmerController.java
@@ -1,2 +1,11 @@
 package com.gangofthree.tarladan.modules.farmer.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/farmer/")
+@RequiredArgsConstructor
+
+public class FarmerController {}
---
+++ backend/src/main/java/com/gangofthree/tarladan/modules/farmer/service/FarmerServiceImpl.java
@@ -0,0 +1,9 @@
+package com.gangofthree.tarladan.modules.farmer.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FarmerServiceImpl implements FarmerService {
+}
